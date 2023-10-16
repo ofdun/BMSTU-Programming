75e43a0be54d3c047a0f39a2622e04057c83f4e3
ofdun <elik394@mail.ru>
2023-10-16 14:05:41 +0300

    laba5

--- laba5/5.py
+++ laba5/5.py
@@ -1,28 +1,49 @@
+# Автор: Калашников Елисей
+# Группа: ИУ7-15Б
+# Вариант 17
+# Назначение программы: вычисление ряда 1/((4n-1)(4n+1))
+# С заданной точностью и шагов
 
-# y = 1/(4n-1)(4n+1)
+number_of_iterations = int(input('Введите количество итераций: '))
+step = int(input('Введите шаг: '))
+eps = float(input('Введите точность: '))
 
-step = int(input())
-number_of_iterations = int(input())
-eps = float(input())
+error = ''
 
-print('-' * 41)
-print('| № Итерации  |', ' '*5 + 't' + ' '*4, '|', ' '*5 + 's' + ' '*5 + '|')
-# t = current; s = sum
-s = 0
-its = 0
-
-
-for i in range(1, number_of_iterations+1):
-    t = 1/((4*i-1)*(4*i+1))
-    s += t
+if number_of_iterations <= 0 or step <= 0 or eps < 0:
+    error = 'Ошибка! Введенные данные должны быть положительными'
     
-    if i % step == 0:
-        print(f'| {i:^12.5g}|{t:^12.5g}|{s:^12.5g}|')
+if not error:
+
+    offset = 41
+
+    print('-' * offset)
+    print('| № Итерации  |     t      |      s     |')
+    print('-' * offset)
+    # t = current; s = sum
+    sum_ = 0
+    its = 0
+    resulted = False
+
+
+    for i in range(1, number_of_iterations+1):
+        current = 1/((4*i-1)*(4*i+1))
+        sum_ += current
         
-    its = i
-    if t < eps:
-        break
-
-print('-' * 41)
-
-print(f'Сумма бесконечного ряда - {s:.5g}, вычислена за {its} итераций.')
+        if (i + step - 1) % step == 0:
+            print(f'| {i:^12.5g}|{current:^12.5g}|{sum_:^12.5g}|')
+            
+        its = i
+        if current < eps:
+            resulted = True
+            break
+    else:
+        print('-' * offset)
+        print(f'Сумму ряда с точностью {eps} невозможно вычислить\
+за заданное количество итераций')
+        
+    if resulted is True:
+        print('-' * offset)
+        print(f'Сумма бесконечного ряда - {sum_:.5g}, вычислена за {its} итераций.')
+else:
+    print(error)
