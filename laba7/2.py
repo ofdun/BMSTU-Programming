faee6259279d86464793bb74351fbac8b1981b36
ofdun <elik394@mail.ru>
2023-10-25 16:32:41 +0300

    done

---
+++ laba7/2.py
@@ -0,0 +1,47 @@
+# while True:
+#     n = float(input('Введите длину списка: '))
+#     if n < 0:
+#         print("Длина списка должна быть больше 0")
+#     elif int(n) != n:
+#         print("Длина списка должна быть целочисленной")
+#     else:
+#         n = int(n)
+#         break
+
+# arr = []
+
+# # Ввод списка
+# for i in range(n):
+#     while True:
+#         elem = float(input(f"Введите {i + 1} элемент массива: "))
+#         if int(elem) != elem:
+#             print("Значение должно быть цесочисленным")
+#         else:
+#             elem = int(elem)
+#             arr.append(elem)
+#             break
+
+arr = [3, 1, -1, 2, -3, 8]
+
+n = len(arr)
+
+k = 0
+for i in range(n):
+    if arr[i] < 0:
+        arr.append(None)
+        k += 1
+        
+new_n = n + k
+        
+for i in range(n-1, -1, -1):
+    if arr[i] >= 0:
+        arr[i + k] = arr[i]
+    else:
+        arr[i + k] = arr[i] * 2
+        arr[i + k - 1] = arr[i]
+        k -= 1
+        
+
+for i in range(new_n):
+    print(arr[i], end=' ')
+print()
