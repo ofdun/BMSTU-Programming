b2b3658fa561aede09baf460b5d9f872b404cfb6
ofdun <elik394@mail.ru>
2023-11-23 18:24:38 +0300

    done

--- laba11/main.py
+++ laba11/main.py
@@ -1,10 +1,40 @@
 from utils.utils import *
+from copy import deepcopy
 
 
 def main():
-    # lst = inputList()
-    # _, lst = gnome_sort(lst)
-    printTable(100, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    WIDTH = 147
+    
+    lst = inputList()
+    lst = [i for i in range(10, -1, -1)]
+    _, _, lst = gnome_sort(lst)
+    printList(lst)
+    
+    n1, n2, n3 = inputInt('1'), inputInt('2'), inputInt('3')
+    
+    orderedListsList = ["Упор. список"]
+    randomListsList = ["Случайный список"]
+    backwardsListsList = ["Уп. в обр. порядке"]
+    
+    for n in n1, n2, n3:
+        randList = createRandomList(n)
+        t, k, orderedRandList = gnome_sort(deepcopy(randList))
+        _, _, backwardsRandList = gnome_sort(deepcopy(randList), reverse=True)
+        
+        t1, k1, _ = gnome_sort(deepcopy(orderedRandList))
+        t2, k2, _ = gnome_sort(deepcopy(backwardsRandList))
+        
+        orderedListsList.extend([t1, k1])
+        randomListsList.extend([t, k])
+        backwardsListsList.extend([t2, k2])
+        
+    printHeader(WIDTH, n1, n2, n3)
+    printTable(WIDTH, [
+        ["", "Время", "Перестановок", "Время", "Перестановок", "Время", "Перестановок"],
+        orderedListsList,
+        randomListsList,
+        backwardsListsList
+        ])
 
 
 if __name__ == '__main__':
--- laba11/utils/utils.py
+++ laba11/utils/utils.py
@@ -1,4 +1,5 @@
 from time import time
+from random import randint
 
 
 def timer(func):
@@ -6,36 +7,42 @@ def timer(func):
         start = time()
         result = func(*args, **kwargs)
         end = time()
-        return f"{(end - start):.5g}", result
+        return f"{(end - start):.5g} секунд", *result
     return wrapper
 
 
 def printTable(w: int, a: [[str]]) -> None:
     print('-'*w)
     for i in range(len(a)):
-        cur_width = w // len(a[i])
+        cur_width = (w - len(a[i])) // len(a[i])
         string = ''
         for j in range(len(a[i])):
-            if isinstance(elem, float):
-                elem = f"{elem:.5g}"
-            string += "|" + f"{elem:^{cur_width}}"
-        print(string + '|')
+            if isinstance(a[i][j], float):
+                a[i][j] = f"{a[i][j]:.5g}"
+            if j == len(a[i]) - 1:
+                string += "|" + f"{a[i][j]:^{cur_width - 1}}|"
+            else:
+                string += "|" + f"{a[i][j]:^{cur_width}}"
+            
+        print(string)
         print('-'*w)
     
 
-
 @timer
-def gnome_sort(lst: list) -> list:
+def gnome_sort(lst: list, reverse=False) -> [int, list]:
+    countShifts = 0
     n, i = len(lst), 1
     while i < n:
         if i == 0:
             i += 1
-        if lst[i] >= lst[i - 1]:
+        if (lst[i] >= lst[i - 1] and not reverse)\
+            or (lst[i] <= lst[i - 1] and reverse):
             i += 1
         else:
             lst[i], lst[i - 1] = lst[i - 1], lst[i]
+            countShifts += 1
             i -= 1
-    return lst
+    return countShifts, lst
 
 
 def inputList() -> list:
@@ -63,13 +70,39 @@ def inputList() -> list:
                 arr.append(elem)
                 break
         
-    # Ввод элемента 
-    while True:
-        elem = float(input(f"Введите элемент, который нужно добавить: "))
-        if int(elem) != elem:
-            print("Значение должно быть цесочисленным")
-        else:
-            elem = int(elem)
-            break
-        
     return arr
+
+
+def printList(lst: list) -> None:
+    print("Итоговый список: ")
+    for c in lst:
+        print(c, end=' ')
+    print()
+    
+    
+def createRandomList(n) -> list[int]:
+    lst = [0]*n
+    for i in range(n):
+        lst[i] = randint(1, 10**10)
+    return lst
+
+
+def inputInt(id: str) -> int:
+    while True:
+        try:
+            n = int(input(f"Введите N{id}: "))
+            if n > 0:
+                return n
+            print("Введенное число должно быть натуральным! ")
+        except ValueError:
+            print("Введенное число должно быть целым! ")
+
+
+def printHeader(w:int, n1: int, n2: int, n3: int) -> None:
+    print('-'*w)
+    print('|' + ' '*20 + '|', end='')
+    new_w = w // 3
+    for n in n1, n2:
+        print(f"{n:^{new_w - 8}.5g}|", end='')
+    print(f"{n:^{new_w - 9}.5g}|", end='')
+    print()
