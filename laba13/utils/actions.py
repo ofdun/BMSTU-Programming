e4d320484fd0932747d153ee57460e1ae5504709
ofdun <elik394@mail.ru>
2023-12-11 22:31:13 +0300

    check if db

--- laba13/utils/actions.py
+++ laba13/utils/actions.py
@@ -5,7 +5,6 @@ from utils.io import (inputDbPath, inputInt,
 
 
 def getIfFileExists(path: str) -> bool:
-    os.path.abspath(__name__)
     if path[-4:] != '.txt':
         return False
     try:
@@ -23,23 +22,47 @@ def getIfFileIsWriteable(path: str) -> bool:
     return os.access(path, os.W_OK)
 
 
+def getIfItIsDB(path: str) -> bool:
+    with open(path, encoding="UTF-8") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                if line.rstrip() != "Животное;Рост(м);Вес(кг)":
+                    return False
+                continue
+            splittedLine = line.split(";")
+            if len(splittedLine) != 3:
+                return False
+            for j in range(1, 3):
+                try:
+                    float(splittedLine[j])
+                except ValueError:
+                    return False            
+    return True
+
+
 def chooseWorkingFile(path: str) -> [str, bool]:
-    if getIfFileExists(path) and getIfFileIsReadable(path):
-        return path, getIfFileIsWriteable(path)
+    fileExists = getIfFileExists(path)
+    fileIsReadable = getIfFileIsReadable(path) if fileExists else False
+    fileIsWriteable = getIfFileIsWriteable(path) if fileExists else False
+    fileIsDb = getIfItIsDB(path) if fileExists and fileIsReadable else False
+    if fileExists and fileIsReadable and fileIsDb:
+        return path, fileIsWriteable
     else:
-        if not getIfFileExists(path):
+        if not fileExists:
             print("Файл не найден!")
-        elif not getIfFileIsReadable(path):
+        elif not fileIsReadable:
             print("Файл не доступен для чтения!")
+        elif not fileIsDb:
+            print("Выбранный файл не является подходящей базой данных для данной программы!")
         return "", False
     
 
 def choosePathToDb(prompt: str) -> str:
     while True:
         pathToDb = inputDbPath(prompt)
-        pathToDb = chooseWorkingFile(pathToDb)
+        pathToDb, writeable = chooseWorkingFile(pathToDb)
         if pathToDb != "":
-            return pathToDb
+            return pathToDb, writeable
         
         
 def initDb(path: str) -> None:
